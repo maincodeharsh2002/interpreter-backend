98f750e91d0959c1ce97ed9cb14493d577a2807b
Harsh Thakur <codeharsh2002@gmail.com>
2024-04-26 21:38:40 +0530

    cors added , changes in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,33 @@
-console.log("Hello World");
-// server/src/app.ts
-import express from "express";
+import express,{ Request,Response } from "express";
 import bodyParser from "body-parser";
 import { Lexer } from "./util/lexer";
 import { Parser } from "./util/parser";
 import { Interpreter } from "./util/interpreter";
+import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(cors());
+
+const PORT = 3001;
 
 app.use(bodyParser.json());
 
-app.post("/evaluate", (req, res) => {
+function interprete(inputString:string): string {
+  const lexer = new Lexer(inputString);
+  const parser = new Parser(lexer);
+  const interpreter = new Interpreter(parser);
+
+  return interpreter.interpret();
+}
+
+// api endpoint to evaluate expression
+app.post("/evaluate", (req:Request,res:Response) => {
   const { expression } = req.body;
   try {
-    const lexer = new Lexer(expression);
-    const parser = new Parser(lexer);
-    const interpreter = new Interpreter(parser);
-    const result = interpreter.interpret();
-    res.send({ result });
-  } catch (error: any) {
-    res.status(400).send({ error: error.message });
+    const result = interprete(expression);
+    res.send({ result });   
+  } catch (error:any) {
+    res.status(400).send({ error });
   }
 });
 
